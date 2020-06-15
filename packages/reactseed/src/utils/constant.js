22a52afbebf7ac98e8b07b1f4e901473acaf8141
zuiidea <zuiiidea@gmail.com>
2020-06-15 14:45:15 +0800

    feat: support new template: antdI18n

--- packages/reactseed/src/utils/constant.js
+++ packages/reactseed/src/utils/constant.js
@@ -1,6 +1,7 @@
 const templateMap = {
   default: '@reactseed/template',
   antd: '@reactseed/template-antd',
+  antdI18n: '@reactseed/template-antd-i18n',
 };
 
 const templateList = [
@@ -12,6 +13,10 @@ const templateList = [
   {
     value: 'antd',
     name: 'Ant Design Template (useRedux + useRequest)',
+  },
+  {
+    value: 'antdI18n',
+    name: 'Ant Design Template (useRedux + useRequest + LinguiJs)',
   },
 ];
 

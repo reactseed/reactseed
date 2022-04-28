adadf09ecfd62e393f6adced4ceb0a2126ce2d36
zuiidea <zuiiidea@gmail.com>
2022-04-28 12:29:05 +0800

    feat: removing use-redux, adding zustand

--- packages/template-antd-i18n/template/src/hooks/index.ts
+++ packages/template-antd-i18n/template/src/hooks/index.ts
@@ -1,6 +1,5 @@
 import { useNavigate, useLocation } from 'react-router-dom';
 import useRequest from '@reactseed/use-request';
-import useRedux from '@reactseed/use-redux';
 import { useLingui } from '@lingui/react';
 import useLocalStorage from './useLocalStorage';
 import useIntlProvider from './useIntlProvider';
@@ -9,7 +8,6 @@ export {
   useNavigate,
   useLocation,
   useRequest,
-  useRedux,
   useLocalStorage,
   useIntlProvider,
   useLingui,
--- packages/template-antd-i18n/template/src/pages/Other/Hello/index.tsx
+++ packages/template-antd-i18n/template/src/pages/Other/Hello/index.tsx
@@ -1,70 +1,31 @@
-import React from 'react';
-import { useRedux } from '@/hooks';
 import { Button, Input } from 'antd';
-import { createStore, Provider } from '@reactseed/use-redux';
+import useStore from './useStore';
 
-interface TState {
-  name: string;
-  age: number;
-}
-
-interface TMethod {
-  updateName: (name: string) => void;
-  becomeOlder: () => void;
-}
-
-const store = createStore(() => ({
-  age: 20,
-  name: 'reactseed',
-}));
-
-const methods = (state: TState): TMethod => {
-  const { age } = state;
-  return {
-    updateName: (name: string) => {
-      state.name = name;
-    },
-    becomeOlder: () => {
-      state.age = age + 1;
-    },
-  };
-};
-
-const Hello: React.FC = () => {
-  const [state, method] = useRedux<TState, TMethod>(methods);
+const Hello = () => {
+  const { age, name, becomeOlder, updateName } = useStore();
 
   return (
     <>
       <p style={{ marginBottom: 16 }}>
-        This is an example using <b>useRedux</b>，See the section about&nbsp;
-        <a href="https://github.com/reactseed/use-redux">
-          @reactseed/use-redux
-        </a>
+        This is an example using <b>zustand</b>，See the section about&nbsp;
+        <a href="https://github.com/pmndrs/zustand">zustand</a>
         &nbsp; for more information.
       </p>
 
       <h1>
-        Hello {state.name} ({state.age})
+        Hello {name} ({age})
       </h1>
 
       <Input
         style={{ width: 240, marginRight: 16 }}
         onChange={e => {
-          method.updateName(e.target.value);
+          updateName(e.target.value);
         }}
-        value={state.name}
+        value={name}
       />
-      <Button onClick={method.becomeOlder}>Older</Button>
+      <Button onClick={becomeOlder}>Older</Button>
     </>
   );
 };
 
-const HelloPage: React.FC = () => {
-  return (
-    <Provider store={store}>
-      <Hello />
-    </Provider>
-  );
-};
-
-export default HelloPage;
+export default Hello;

f1a75932189ff4fb364733ccd3b52fc9038ebf09
zuiidea <zuiiidea@gmail.com>
2022-05-18 16:45:11 +0800

    refactor: upgrading husky

--- packages/reactseed/src/utils/constant.js
+++ packages/reactseed/src/utils/constant.js
@@ -28,6 +28,7 @@ const templateList = [
 const registry = 'https://registry.npmjs.org';
 const registryCN = 'https://registry.npm.taobao.org';
 const downloadTempPath = '.reactseed';
+
 module.exports = {
   templateMap,
   registry,
